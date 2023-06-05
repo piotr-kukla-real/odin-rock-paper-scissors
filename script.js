6ecbf4504481f75279d12fd47a2dc14e4afb744c
Piotr Kukla <thepiotrkukla@gmail.com>
2023-06-05 09:45:07 +0200

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,21 @@
+const ROCK = "ROCK",
+      PAPER = "PAPER",
+      SCISSORS = "SCISSORS";
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    let choice;
+
+    switch (random) {
+        case 0:
+            choice = ROCK;
+            break;
+        case 1:
+            choice = PAPER;
+            break;
+        default:
+            choice = SCISSORS;
+        }
+
+    return choice;
+}
