07d250c391e96d7907cdc1a32f69fab3bfc56a45
Piotr Kukla <thepiotrkukla@gmail.com>
2023-06-05 09:59:49 +0200

    Create getPlayerChoice function

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ const checkSelection = selection =>
     selection == ROCK ||
     selection == PAPER ||
     selection == SCISSORS;
+
+// get player choice, loop until it is valid, return null if canceled
+function getPlayerChoice() {
+    let promptMessage = "Your pick";
+
+    while (true) {
+        let choice = prompt(promptMessage);
+
+        if (choice === null) {
+            return null;
+        }
+
+        choice = choice.toUpperCase();
+        const isValid = checkSelection(choice);
+        if (isValid) {
+            return choice;
+        }
+
+        promptMessage = "You can pick only ROCK, PAPER or SCISSORS";
+    }
+}
